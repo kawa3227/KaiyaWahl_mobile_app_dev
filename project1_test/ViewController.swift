cdd985409f570204cb37e9a13a3685314098bf8c
kawa3227 <65365689+kawa3227@users.noreply.github.com>
2021-10-07 10:29:23 -0600

    Update ViewController.swift

--- project1_test/ViewController.swift
+++ project1_test/ViewController.swift
@@ -8,45 +8,95 @@
 import UIKit
 import AVFoundation
 class ViewController: UIViewController {
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var minStepper: UIStepper!
+    @IBOutlet weak var timeCounter: UILabel!
+    
+    @IBAction func timerStepper(_ sender: UIStepper) {
+        if minStepper.value == 1 {
+            timeCounter.text = "1 minute"
+        }
+        else{
+            timeCounter.text = String(format: "%.0f", minStepper.value) + " minutes"
+        }
+    }
     
     var OutTimer = Timer()
     var TimerDisplayer = 0
-    
-    @IBOutlet weak var Label: UILabel!
- 
-    
     @objc func Action(){
+        //TimerDisplayer = Int(minStepper.value * 60)
         TimerDisplayer += 1
         Label.text = String(TimerDisplayer)
+        if TimerDisplayer == Int(minStepper.value * 60){
+            audioPlayer.stop()
+           // Label.text = "Your meditation as ended"//replace wit alert
+            let alert = UIAlertController(title: "Warning", message: "You mediation as ended", preferredStyle: UIAlertController.Style.alert)
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel, handler: nil)
+            alert.addAction(cancelAction)
+                        
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {action in
+                self.OutTimer.invalidate()
+                self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                self.Label.text = "0"
+                
+                        })
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            
+        }
+
+    }
+
+    @IBAction func choose_background(_ sender: UIButton) {
+        if sender.tag == 4 {
+            self.view.backgroundColor = #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)
+        }
+        else if sender.tag == 5 {
+            self.view.backgroundColor = #colorLiteral(red: 0.659389317, green: 0.8405041099, blue: 1, alpha: 1)
+        }
+        else if sender.tag == 6 {
+            self.view.backgroundColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1)
+        }
     }
     var audioPlayer = AVAudioPlayer()
-   
+    @IBAction func chooseSound(_ sender: UIButton) {
+        if sender.tag == 0{
+            let sound = Bundle.main.path(forResource: "Ocean", ofType: "mp3")
+            do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath:sound!))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode:AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
+            } catch {
+            }
+        }
+        else if sender.tag == 1{
+            let sound = Bundle.main.path(forResource: "Creek", ofType: "mp3")
+            do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath:sound!))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode:AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
+            } catch {
+            }
+        }
+        else if sender.tag == 3 {
+            let sound = Bundle.main.path(forResource: "Rain", ofType: "mp3")
+            do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath:sound!))
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode:AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
+            } catch {
+            }
+        }
+    }
+
+    @IBAction func playButtonPressed(_ sender: UIButton) {
+        audioPlayer.numberOfLoops = -1
+        audioPlayer.play()
+        OutTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let sound = Bundle.main.path(forResource: "Ocean", ofType: "mp3")
-        do {
-        audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath:sound!))
-        try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode:AVAudioSession.Mode.default, options: [AVAudioSession.CategoryOptions.mixWithOthers])
-        } catch {
-            print(error)
-        }
-
-
-    }
-    @IBAction func playButtonPressed(_ sender: UIButton) {
-        audioPlayer.play()
-        OutTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(Action), userInfo: nil, repeats: true)
     }
    
 }
 
-//question 1 what are your intentions answers relax, release, energy, thought
-// if user chooses relax change BG color to blue energy - yellow thought -purple release -orange
-//quesition 2 what sounds would you like to listen to answers: creek, ocean, wind
-//if user chooses creek play creek sounds
-//question 3 how long would you like to meditate for
-//if user says 3 minutes
-//set max to 3 mintues or 180 seconds and have time give an alert when it reaches the max 
-//each option for questions 1-2 will be their own button with their own actions 
